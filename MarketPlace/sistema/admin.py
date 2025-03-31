cc7d1ed2a4f90493058dcbcfa7bd782aadef0e73
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-03-31 19:28:07 -0300

    Adição da tabela usurios

--- MarketPlace/sistema/admin.py
+++ MarketPlace/sistema/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Usuarios
+
+class listaUsuario(admin.ModelAdmin):
+    # list_display = ("usuario", "nm_doador", "cpf", "cep")
+    # list_display_links = ("usuario", "nm_doador")
+    # search_fields = ("nm_doador",)
+    list_per_page = 10
+
 
 # Register your models here.
+admin.site.register(Usuarios,listaUsuario)
--- MarketPlace/sistema/models.py
+++ MarketPlace/sistema/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Usuarios(models.Model):
+    usuario = models.OneToOneField(User,on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=11,null=False,blank=False,unique=True)
+    telefone = models.CharField(max_length=13, null=False, blank=False, unique=True)
+    cep = models.CharField(max_length=9, null=False, blank=False)
+    #historico_doacoes = models.IntegerField(default=0)
+    complemento = models.CharField(max_length=255, null=False, blank=False)
+def __str__(self):
+    return self.usuario
 
 
